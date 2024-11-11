1ee5813047d52cbf49b99c40ea73913b9bbb8d99
wojnareva <126489853+wojnareva@users.noreply.github.com>
2024-11-11 17:52:59 +0100

    life in weeks project for Angela web

---
+++ lifeInWeeks.js
@@ -0,0 +1,14 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var remainingYears = 90 - age
+        var z = remainingYears * 12
+        var y = remainingYears * 52
+        var x = remainingYears * 365
+        
+     console.log("You have "+x+" days, "+y+" weeks, and "+z+" months left.")   
+    /*************Don't change the code below**********/
+    }
+    
